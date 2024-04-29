71a867c1dcd0394dad9fba57860199e4a23ef649
Michael Heilmann <michael.heilmann@shizu.org>
2024-04-29 05:36:00 +0200

    add `Shizu_getWorkingDirectory`

---
+++ Library/includes/Shizu/Runtime/getWorkingDirectory.h
@@ -0,0 +1,39 @@
+/*
+  Shizu Runtime
+  Copyright (C) 2024 Michael Heilmann. All rights reserved.
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#if !defined(SHIZU_RUNTIME_GETWORKINGDIRECTORY_H_INCLUDED)
+#define SHIZU_RUNTIME_GETWORKINGDIRECTORY_H_INCLUDED
+
+#if !defined(SHIZU_RUNTIME_PRIVATE) && 1 != SHIZU_RUNTIME_PRIVATE
+  #error("Do not include `Shizu/Runtime/getWorkingDirectory.h` directly. Include `Shizu/Runtime/Include.h` instead.")
+#endif
+
+#include "Shizu/Runtime/Configure.h"
+typedef struct Shizu_State Shizu_State;
+typedef struct Shizu_String Shizu_String;
+
+Shizu_String*
+Shizu_getWorkingDirectory
+  (
+    Shizu_State* state
+  );
+
+#endif // SHIZU_RUNTIME_GETWORKINGDIRECTORY_H_INCLUDED
---
+++ Library/sources/Shizu/Runtime/getWorkingDirectory.c
@@ -0,0 +1,67 @@
+/*
+  Shizu Runtime
+  Copyright (C) 2024 Michael Heilmann. All rights reserved.
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#define SHIZU_RUNTIME_PRIVATE (1)
+#include "Shizu/Runtime/getWorkingDirectory.h"
+
+#include "Shizu/Runtime/State.h"
+#include "Shizu/Runtime/Objects/String.h"
+
+#include "idlib/file_system.h"
+
+typedef struct Context {
+  Shizu_State* state;
+  Shizu_String* path;
+} Context;
+
+static bool
+callback
+  (
+    Context* context,
+    void const* bytes,
+    size_t numberOfBytes
+  )
+{
+  Shizu_JumpTarget jumpTarget;
+  Shizu_State_pushJumpTarget(context->state, &jumpTarget);
+  if (!setjmp(jumpTarget.environment)) {
+    context->path = Shizu_String_create(context->state, bytes, numberOfBytes);
+  }
+  Shizu_State_popJumpTarget(context->state);
+  return true;
+}
+
+Shizu_String*
+Shizu_getWorkingDirectory
+  (
+    Shizu_State* state
+  )
+{
+  Context context = {
+    .state = state,
+    .path = NULL,
+  };
+  if (idlib_get_working_directory(&context, &callback)) {
+    Shizu_State_setStatus(state, Shizu_Status_EnvironmentFailed);
+    Shizu_State_jump(state);
+  }
+  return context.path;
+}
